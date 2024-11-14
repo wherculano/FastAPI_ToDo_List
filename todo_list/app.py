7075eab19b6c9e5a46926e398ff94dd11401d807
Wagner Herculano <wagherculano@hotmail.com>
2024-11-14 15:55:19 -0300

    Adicionado schema para documentacao de "contrato"

--- todo_list/app.py
+++ todo_list/app.py
@@ -1,8 +1,12 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from todo_list.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', response_model=Message, status_code=HTTPStatus.OK)
 def read_root():
     return {'message': 'Olá Mundo!'}

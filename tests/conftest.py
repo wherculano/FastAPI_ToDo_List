65fa9a3af516b9e9a1b7547ba286b58ac0f4a387
Wagner Herculano <wagherculano@hotmail.com>
2024-12-04 00:48:54 -0300

    Adicionadas novas fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,19 +5,32 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
+from sqlalchemy.pool import StaticPool
 
 from todo_list.app import app
-from todo_list.models import table_registry
+from todo_list.database import get_session
+from todo_list.models import User, table_registry
 
 
 @pytest.fixture
-def client():
-    return TestClient(app)
+def client(session):
+    def get_fake_session():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_fake_session
+        yield client
+
+    app.dependency_overrides.clear()
 
 
 @pytest.fixture
 def session():
-    engine = create_engine('sqlite:///:memory:')
+    engine = create_engine(
+        'sqlite:///:memory:',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
     table_registry.metadata.create_all(engine)
 
     with Session(engine) as session:
@@ -44,3 +57,13 @@ def _mock_db_time(*, model, time=datetime(2024, 1, 1)):
 @pytest.fixture
 def mock_db_time():
     return _mock_db_time
+
+
+@pytest.fixture
+def user(session):
+    user = User(username='Teste', email='test@test.com', password='testtest')
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    return user
